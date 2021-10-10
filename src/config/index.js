ffdf96ca5eb879c752e333451ae0f0ec7b5e544c
youssef azizi <aziziyoussef97@gmail.com>
2021-10-10 16:52:46 +0100

    test commit

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+export default {
+  clientid: '250984f7-a56b-4262-a47b-20cde9c3b8fc',
+  redirecturl: 'http://localhost:8081',
+  authority: 'https://login.microsoftonline.com/4e93e72b-a813-46fe-8780-207b3bcddfa4',
+  graphscopes: ['user.read'],
+  graphendpoint: 'https://graph.microsoft.com/v1.0/me',
+  appinsightsid: ''
+}
---
+++ src/views/charts/LineExample.vue
@@ -0,0 +1,53 @@
+<script>
+import { Line } from 'vue-chartjs'
+import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips'
+import { hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities'
+
+export default {
+  components: {
+    hexToRgba,
+    CustomTooltips
+  },
+  extends: Line,
+  mounted () {
+    this.renderChart(
+      {
+        labels: 'http://api.openapi-generator.tech/api/gen/clients',
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: hexToRgba('#E46651', 90),
+            data: [30, 39, 10, 50, 30, 70, 35]
+          },
+          {
+            label: 'Data Two',
+            backgroundColor: hexToRgba('#00D8FF', 90),
+            data: [39, 80, 40, 35, 40, 20, 45]
+          },
+          {
+            label: 'Data three',
+            backgroundColor: hexToRgba('#41B883', 90),
+            data: [0, 10, 20, 30, 40, 50, 45]
+          }
+        ]
+      },
+      {
+        responsive: true,
+        maintainAspectRatio: true,
+        tooltips: {
+          enabled: false,
+          custom: CustomTooltips,
+          intersect: true,
+          mode: 'index',
+          position: 'nearest',
+          callbacks: {
+            labelColor: function (tooltipItem, chart) {
+              return { backgroundColor: chart.data.datasets[tooltipItem.datasetIndex].backgroundColor }
+            }
+          }
+        }
+      }
+    )
+  }
+}
+</script>
